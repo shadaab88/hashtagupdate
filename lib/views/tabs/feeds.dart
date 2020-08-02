df8cc994790dea46c43527ab505c5dcfbb12c159
shadaab88 <abdulshadaab4646@gmail.com>
2020-08-02 19:18:32 +0530

    profile section added

--- lib/views/tabs/feeds.dart
+++ lib/views/tabs/feeds.dart
@@ -35,6 +35,101 @@ class _HomePageState extends State<FeedsPage> {
       body: Container(
           child: ListView(
             children: <Widget>[
+
+              // profile section start
+          Container(
+            color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 0.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              //SizedBox(height: 5),
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  "assets/images/profile6.jpg",
+                  //"assets/cm${random.nextInt(10)}.jpeg",
+                ),
+                radius: 30,
+              ),
+              SizedBox(height: 10),
+              Text(
+                "abdul shadaab",
+                //names[random.nextInt(10)],
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 6),
+//            Text(
+//              "Status should be here",
+//              style: TextStyle(),
+//            ),
+//            SizedBox(height: 20),
+//            Row(
+//              mainAxisSize: MainAxisSize.min,
+//              children: <Widget>[
+//                FlatButton(
+//                  child: Icon(
+//                    Icons.message,
+//                    color: Colors.white,
+//                  ),
+//                  color: Colors.grey,
+//                  onPressed: () {},
+//                ),
+//                SizedBox(width: 10),
+//                FlatButton(
+//                  child: Icon(
+//                    Icons.add,
+//                    color: Colors.white,
+//                  ),
+//                  color: Theme.of(context).accentColor,
+//                  onPressed: () {},
+//                ),
+//              ],
+//            ),
+//            SizedBox(height: 40),
+//            Padding(
+//              padding: EdgeInsets.symmetric(horizontal: 50),
+//              child: Row(
+//                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                children: <Widget>[
+//                  _buildCategory("Posts"),
+//                  _buildCategory("Friends"),
+//                  _buildCategory("Groups"),
+//                ],
+//              ),
+//            ),
+//            SizedBox(height: 20),
+//            GridView.builder(
+//              shrinkWrap: true,
+//              physics: NeverScrollableScrollPhysics(),
+//              primary: false,
+//              padding: EdgeInsets.all(5),
+//              itemCount: 15,
+//              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                crossAxisCount: 3,
+//                childAspectRatio: 200 / 200,
+//              ),
+//              itemBuilder: (BuildContext context, int index) {
+//                return Padding(
+//                  padding: EdgeInsets.all(5.0),
+//                  child: Image.asset(
+//                    "assets/cm${random.nextInt(10)}.jpeg",
+//                    fit: BoxFit.cover,
+//                  ),
+//                );
+//              },
+//            ),
+            ],
+          ),
+        ),
+      ),
+
+              //profile section end
               Column(
                 children: <Widget> [
 //                  Container(
